70b1d6acd5356d283211d8aa4d63f7d4591bbad8
waserende <916040853@qq.com>
2021-06-11 17:33:22 +0800

    添加配置文件

--- common/database.go
+++ common/database.go
@@ -4,19 +4,20 @@ import (
 	"fmt"
 
 	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
 	"waserd.ren/myBlog/model"
 )
 
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	driveName := "mysql"
-	host := "localhost"
-	port := "3306"
-	database := "ginBlog"
-	username := "root"
-	password := "123123"
-	charset := "utf8"
+	driveName := viper.GetString("datasource.driveName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
 		username,
 		password,
--- go.mod
+++ go.mod
@@ -14,7 +14,7 @@ require (
 	github.com/mattn/go-isatty v0.0.13 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
-	github.com/spf13/viper v1.7.1 // indirect
+	github.com/spf13/viper v1.7.1
 	github.com/ugorji/go v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a
 	golang.org/x/sys v0.0.0-20210608053332-aa57babbf139 // indirect
